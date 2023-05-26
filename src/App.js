caf9b8534d953b14860c977f5021a131a5f86e0c
hy-qqqqq <amy.s.perng@gmail.com>
2023-05-27 03:35:23 +0800

    test(app): add & fix menu item features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import logo from "./logo.svg"
 import './App.css';
+import { useState } from 'react';
+import { Button, Layout, Menu } from 'antd';
+import { 
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  HomeOutlined,
+  StarOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+const { Header, Footer, Sider } = Layout;
+
+function AppSider() {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <Sider data-testid='sider' theme='light' collapsed={collapsed} >
+      <Button data-testid='button' onClick={toggleCollapsed} style={{ margin: 10, width: "-webkit-fill-available" }}>
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </Button>
+      <Menu items={[
+        {label: "Home", icon: <HomeOutlined />},
+        {label: "Favorite", icon: <StarOutlined />},
+        {label: "Account", icon: <UserOutlined />}
+      ]}>
+      </Menu>
+    </Sider>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Layout className='App'>
+      <Header className="Header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Header>
+      <Layout className='Content'>
+        <AppSider />
+      </Layout>
+      <Footer className='Content'>
+        Software Testing Final Project @ 2023
+      </Footer>
+    </Layout>
   );
 }
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,72 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/*
+  [Test case conventions]
+  - format: <Feature> <Scenario> <Expected Result>
+  - normal test steps:
+    1. arrange: render
+    2. act:     user events & fetch elements
+    3. assert:  compared elements with expected result
+*/
+
+/*
+  [App features]
+  1. show menu on the side
+    1.1 menu item should have length of 3
+    1.2 menu item contains text: home, fav, acc
+    1.3 menu item focus
+    1.4 menu is not collapsed in default
+    1.5 menu can be set collapsed with the toggle button
+  2. create routes to pages: home, fav, acc
+    2.1 check url changes
+*/
+
+const targetMenu = [
+  {name: 'Home'},
+  {name: 'Favorite'},
+  {name: 'Account'}
+];
+
+test('Menu-item is displayed and show text: home, fav, acc', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const menuItemEles = await screen.findAllByRole('menuitem');
+  expect(menuItemEles.length).toEqual(targetMenu.length);
+  menuItemEles.forEach((e, i) => {
+    expect(e).toHaveTextContent(targetMenu[i].name);
+  });
+});
+
+test('Menu-item which is focused should have the focus attribute', async () => {
+  render(<App />);
+  const menuItemEles = await screen.findAllByRole('menuitem');
+  menuItemEles.forEach((e) => {
+    act(() => e.focus());
+    expect(e).toHaveFocus();
+  });
+});
+
+test('Toggle-button onclick can set the collapsed attribute of the sider', async () => {
+  render(<App />);
+  const buttonEle = await screen.findByTestId('button');
+  const siderEle = await screen.findByTestId('sider');
+  // default: not collapsed
+  expect(siderEle).not.toHaveAttribute('class', expect.stringContaining('collapsed'));
+  // first click: collapsed
+  fireEvent.click(buttonEle);
+  expect(siderEle).toHaveAttribute('class', expect.stringContaining('collapsed'));
+  // second click: not collapsed
+  fireEvent.click(buttonEle);
+  expect(siderEle).not.toHaveAttribute('class', expect.stringContaining('collapsed'));
+});
+
+test('Sider is collapsed and not showing any text', async () => {
+  render(<App />);
+  const buttonEle = await screen.findByTestId('button');
+  // hide menu
+  fireEvent.click(buttonEle);
+  targetMenu.forEach((e) => {
+    const q = screen.queryByText(e.name);
+    expect(q).not.toBeVisible();
+  });
 });
