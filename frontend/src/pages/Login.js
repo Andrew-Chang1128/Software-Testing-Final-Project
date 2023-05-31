29d19c2ed0b03e4def1540b822e9b9b187230c1a
hy-qqqqq <amy.s.perng@gmail.com>
2023-05-31 20:40:28 +0800

    test(login): add & fix register form and fetch api

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,84 @@
-import { Row, Col, Input, Form, Button, message } from 'antd';
+import { Row, Col, Input, Form, Button, message, Modal, Divider, Typography } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { fetchUnAuth } from '../utils';
+import { useState } from 'react';
+
+function CMField({ fieldName, type, Icon }) {
+  return (
+    <Form.Item
+      name={fieldName}
+      label={fieldName}
+      rules={[
+        {
+          required: true,
+          message: `Please input your ${fieldName}!`,
+        },
+      ]}
+    >
+      <Input
+        prefix={Icon}
+        autoComplete="off"
+        type={type}
+      />
+    </Form.Item>
+  );
+};
+
+function CMForm({ formName, onFinish }) {
+  return (
+    <Form
+      name={formName}
+      onFinish={onFinish}
+      preserve={false}
+      requiredMark={false}
+    >
+      <CMField fieldName={"username"} type="text" Icon={<UserOutlined />}/>
+      <CMField fieldName={"password"} type="password" Icon={<LockOutlined />}/>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" style={{width: '100%'}}>
+          {formName}
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+function RegisterForm({ open, hideModal }) {
+  const [msg, msgHolder] = message.useMessage();
+  const displayError = () => {
+    msg.open({
+      type: 'error',
+      content: 'Failed to register!',
+    });
+  };
+  const displaySuccess = () => {
+    msg.open({
+      type: 'success',
+      content: 'Success!',
+    });
+  };
+  const onFinish = (values) => {
+      fetchUnAuth('/user/createUser', values)
+        .then((res) => {displaySuccess()})
+        .catch((error) => {displayError()});
+  };
+
+  return (
+    <Modal
+      title="Register"
+      open={open}
+      onCancel={hideModal}
+      footer={null}
+      destroyOnClose
+    >
+      {msgHolder}
+      <CMForm
+        formName="Register"
+        onFinish={onFinish}
+      />
+    </Modal>
+  );
+};
 
 function LoginForm({ setToken }) {
   const [msg, msgHolder] = message.useMessage();
@@ -10,66 +88,39 @@ function LoginForm({ setToken }) {
       content: 'Failed to login!',
     });
   };
+  const onFinish = (values) => {
+      fetchUnAuth('/user/login', values)
+        .then((res) => {setToken(res);})
+        .catch((error) => {displayError()});
+  };
 
   return (
-    <Form
-      name="login"
-      requiredMark={false}
-      onFinish={(values) => {
-        fetchUnAuth('/user/login', values)
-          .then((res) => {setToken(res);})
-          .catch((error) => {displayError()});
-      }}
-    >
+    <>
+      <Typography.Title level={3} style={{padding: '10px'}}>Login</Typography.Title>
       {msgHolder}
-      <Form.Item
-        name="username"
-        label="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined />}
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        label="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined />}
-          type="password"
-          autoComplete="off"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" style={{width: '100%'}}>
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
+      <CMForm
+        formName="Login"
+        onFinish={onFinish}
+      />
+    </>
   );
 }
 
 function Auth({ setToken }) {
+  const [open, setOpen] = useState(false);
+  const openModal = () => {setOpen(true)};
+  const hideModal = () => {setOpen(false)};
   return(
     <Row align={'middle'} justify={'center'} style={{minHeight: '100vh'}}>
-      <Col>
+      <Col align={'center'}>
         <LoginForm setToken={setToken}/>
+        <Divider plain>or</Divider>
+        <Button onClick={openModal} style={{width: '100%'}}>Create Account</Button>
+        <RegisterForm open={open} hideModal={hideModal} />
       </Col>
     </Row>
   );
 };
 
 export default Auth;
-export { LoginForm };
+export { LoginForm, RegisterForm, CMForm };
--- frontend/src/pages/Login.test.js
+++ frontend/src/pages/Login.test.js
@@ -1,5 +1,5 @@
 import { render, screen, act, fireEvent } from '@testing-library/react';
-import Auth, { LoginForm } from './Login';
+import Auth, { LoginForm, RegisterForm, CMForm } from './Login';
 import * as utils from '../utils';
 
 afterEach(() => {
@@ -7,10 +7,55 @@ afterEach(() => {
 });
 
 describe('Auth', () => {
+  beforeEach(() => {
+    render(<Auth setToken={jest.fn()}/>);
+  });
+
   describe('in the initial', () => {
     test('should render the login form', () => {
-      render(<Auth setToken={jest.fn()}/>);
       expect(screen.getByRole('button', { name: 'Login' })).toBeTruthy();
+    });
+  });
+  
+  describe('click the register button', () => {
+    beforeEach(() => {
+      const openBtn = screen.getByRole('button', {name: 'Create Account'});
+      fireEvent.click(openBtn);
+    });
+
+    test('should render the registration form modal', async () => {
+      expect(await screen.findByRole('button', {name: 'Register'})).toBeTruthy();
+    });
+
+    test('click the close button should close the modal', async () => {
+      const closeBtn = await screen.findByRole('button', {name: 'Close'});
+      fireEvent.click(closeBtn);
+      expect(await screen.queryByRole('button', {name: 'Register'})).toBeNull();
+    });
+  });
+});
+
+describe('Form', () => {
+  let BtnEle;
+  beforeEach(() => {
+    render(<CMForm formName="Test" onFinish={jest.fn()}/>);
+    BtnEle = screen.getByRole('button', { name: 'Test' });
+  });
+
+  describe('submit with empty fields', () => {
+    test('should show required messages', async () => {
+      fireEvent.submit(BtnEle);
+      expect(await screen.findByText('Please input your username!')).toBeVisible();
+      expect(await screen.findByText('Please input your password!')).toBeVisible();
+    });
+
+    test('should not fetch api', async () => {
+      jest
+        .spyOn(utils, 'fetchUnAuth');
+      await act(async () => {
+        fireEvent.submit(BtnEle)
+      });
+      expect(utils.fetchUnAuth).not.toHaveBeenCalled();
     });
   });
 });
@@ -24,23 +69,6 @@ describe('Login form', () => {
     loginBtn = screen.getByRole('button', { name: 'Login' });
     usernameInput = screen.getByLabelText(/username/i);
     passwordInput = screen.getByLabelText(/password/i);
-  });
-
-  describe('submit with empty fields', () => {
-    test('should show required messages', async () => {
-      fireEvent.submit(loginBtn);
-      expect(await screen.findByText('Please input your username!')).toBeVisible();
-      expect(await screen.findByText('Please input your password!')).toBeVisible();
-    });
-
-    test('should not fetch login api', async () => {
-      jest
-        .spyOn(utils, 'fetchUnAuth');
-      await act(async () => {
-        fireEvent.submit(loginBtn)
-      });
-      expect(utils.fetchUnAuth).not.toHaveBeenCalled();
-    });
   });
   
   describe('submit with valid username and password', () => {
@@ -57,7 +85,7 @@ describe('Login form', () => {
 
       expect(utils.fetchUnAuth).toHaveBeenCalledTimes(1);
       expect(mockSetToken).toHaveBeenCalledWith('testtoken');
-    })
+    });
   });
 
   describe('submit with invalid username or password', () => {
@@ -73,6 +101,49 @@ describe('Login form', () => {
       expect(utils.fetchUnAuth).toHaveBeenCalledTimes(1);
       expect(await screen.findByText('Failed to login!'));
     });
-  })
+  });
 });
 
+describe('Registration form', () => {
+  let regBtn, usernameInput, passwordInput;
+  beforeEach(() => {
+    render(<RegisterForm open={true} hideModal={jest.fn()}/>);
+    regBtn = screen.getByRole('button', { name: 'Register' });
+    usernameInput = screen.getByLabelText(/username/i);
+    passwordInput = screen.getByLabelText(/password/i);
+  });
+
+  describe('submit with valid username and password', () => {
+    test('should show success message', async () => {
+      jest
+        .spyOn(utils, 'fetchUnAuth')
+        .mockResolvedValueOnce(Promise.resolve([]));
+  
+      await act(async () => {
+        fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+        fireEvent.change(passwordInput, { target: { value: '12345678' } });
+        fireEvent.submit(regBtn);
+      })
+
+      expect(utils.fetchUnAuth).toHaveBeenCalledTimes(1);
+      expect(await screen.findByText('Success!'));
+    })
+  });
+
+  describe('submit with invalid username and password', () => {
+    test('should show error message', async () => {
+      jest
+        .spyOn(utils, 'fetchUnAuth')
+        .mockRejectedValueOnce(Error('test: invalid registeration'));
+  
+      await act(async () => {
+        fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+        fireEvent.change(passwordInput, { target: { value: '00000000' } });
+        fireEvent.submit(regBtn);
+      })
+
+      expect(utils.fetchUnAuth).toHaveBeenCalledTimes(1);
+      expect(await screen.findByText('Failed to register!'));
+    });
+  });
+});
